d5439432ec25f4fbbcc928f8fc6d1ad07d9ba7e1
bigetion <shinichi.dconnan@gmail.com>
2018-04-27 02:14:38 +0800

    CRUD Pembelian

--- src/components/pages/Pembelian.vue
+++ src/components/pages/Pembelian.vue
@@ -1,25 +1,119 @@
 <template>
   <div>
-    <v-toolbar class="grey lighten-3" light flat v-if="user.authenticated">
+    <v-toolbar class="grey lighten-3" light flat>
       <v-toolbar-title>Pembelian</v-toolbar-title>
     </v-toolbar>
+    <div>
+      <div v-if="state.isAdd || state.isEdit">
+        <add-pembelian 
+          :data="state.isAdd ? {}: this.selectedRow[0]" 
+          :is-edit="state.isEdit || !state.isAdd"
+          :on-cancel="onCancel"
+          :on-submitted="onSubmitted"
+        ></add-pembelian>
+      </div>
+      <div v-show="!(state.isAdd || state.isEdit)">
+        <v-card flat>
+          <v-layout row wrap>
+            <v-flex md12 class="pa-3">
+              <!-- <v-text-field solo label="Search" append-icon="search" class="mb-2" v-model="q"></v-text-field> -->
+              <table-view 
+                from-file="pembelian" 
+                :is-refresh="state.isRefresh"
+                :is-delete="state.isDelete"
+                :is-download="state.isDownload"
+                :on-select="onSelect"
+              ></table-view>
+            </v-flex>
+          </v-layout>
+        </v-card>
+        <v-divider></v-divider>
+        <v-card flat class="pa-2">
+          <v-btn success @click="setIsAdd(true)">Tambah</v-btn>
+          <v-btn dark class="deep-orange float-right" v-if="selectedRow.length==1 && selectedRow[0]['approved']=='1'" @click="onCetak()">Cetak</v-btn>
+          <v-btn dark class="teal float-right" v-if="selectedRow.length==1 && selectedRow[0]['approved']=='0' && user.idRole=='1'" default @click="onApprove()">Approve</v-btn>
+        </v-card>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import auth from "./../../utils/auth"
+import { modal } from "./../../utils/modal";
+import { confirm } from "./../../utils/modal";
+import auth from "./../../utils/auth";
+
+import AddPembelian from "./forms/AddPembelian.vue";
+import FormCetak from "./forms/FormCetak.vue";
+
 export default {
+  components: { AddPembelian },
   data: () => ({
-    user: auth.user
+    user: auth.user,
+    state: {
+      isAdd: false,
+      isEdit: false,
+      isRefresh: false,
+      isDownload: false
+    },
+    q: "",
+    selectedRow: [],
+    input: {
+      nama: ""
+    }
   }),
-  methods: {},
-  created() {}
+  created() {},
+  methods: {
+    onSelect(row) {
+      this.selectedRow = row;
+    },
+    setIsAdd(condition) {
+      this.state.isAdd = condition;
+    },
+    setIsEdit(condition) {
+      this.state.isEdit = condition;
+    },
+    onCancel() {
+      this.setIsAdd(false);
+      this.setIsEdit(false);
+    },
+    onSubmitted() {
+      this.onCancel();
+      this.state.isRefresh = !this.state.isRefresh;
+    },
+    onApprove() {
+      confirm(this, {
+        title: "Konfirmasi Approve",
+        message: "Apakah anda yakin?"
+      }).then(() => {
+        this.$http
+          .post("base/service/executeMutation", {
+            id: this.selectedRow[0]["id"],
+            type: "update",
+            name: "pembelian",
+            data: {
+              approved: 1
+            }
+          })
+          .then(() => {
+            this.state.isRefresh = !this.state.isRefresh;
+          });
+      });
+    },
+    onCetak() {
+      modal(this, {
+        name: "full-modal",
+        props: {
+          data: this.selectedRow[0]
+        },
+        dismissable: false,
+        component: FormCetak
+      }).then(response => {});
+    }
+  }
 };
 </script>
 
 <style lang="css">
-.carousel__item {
-  background-size: inherit;
-  background-position: center top;
-}
+
 </style>
---
+++ src/components/pages/forms/AddPembelian.vue
@@ -0,0 +1,160 @@
+<template>
+  <div>
+    <v-form v-model="valid" ref="form">
+      <v-card flat class="pa-4">
+        <v-layout row wrap>
+          <v-flex md6 class="px-2">
+            <v-text-field label="Nomor PO" name="Nomor PO" v-model="input.nomorPO" :error-messages="errors.collect('Nomor PO')" v-validate="'required'"></v-text-field>
+          </v-flex>
+          <v-flex md6 class="px-2">
+            <datepicker 
+              label="Tanggal" 
+              name="Tanggal" 
+              v-model="input.tanggal" 
+              :error-messages="errors.collect('Tanggal')" 
+              v-validate="'required'" 
+              value-format="DD MMMM YYYY"
+            ></datepicker>
+          </v-flex>
+          <v-flex md6 class="px-2">
+            <v-text-field label="Status Bayar" name="Status Bayar" v-model="input.statusBayar" :error-messages="errors.collect('Status Bayar')" v-validate="'required'"></v-text-field>
+          </v-flex>
+          <v-flex md6 class="px-2">
+            <v-text-field label="QTY (Kiloliter)" name="QTY" v-model="input.qty" :error-messages="errors.collect('QTY')" v-validate="'required'"></v-text-field>
+          </v-flex>
+          <v-flex md6 class="px-2">
+            <v-text-field label="Harga / Liter" name="Harga" v-model="input.harga" :error-messages="errors.collect('Harga')" v-validate="'required'"></v-text-field>
+          </v-flex>
+          <v-flex md6 class="px-2">
+            <v-select
+              :items="['Cash','Kredit']"
+              v-model="input.jenisPembayaran"
+              label="Jenis Pembayaran"
+              name="Jenis Pembayaran"
+              :error-messages="errors.collect('Jenis Pembayaran')" 
+              v-validate="'required'"
+            ></v-select>
+          </v-flex>
+          <v-flex md6 class="px-2">
+            <v-text-field label="TOP" name="TOP" v-model="input.top" :error-messages="errors.collect('TOP')" v-validate="'required'"></v-text-field>
+          </v-flex>
+          <v-flex md6 class="px-2 detail">
+            <v-text-field label="Jatuh Tempo" :value="jatuhTempoFormat" :disabled="true"></v-text-field>
+          </v-flex>
+        </v-layout>
+      </v-card>
+      <v-divider></v-divider>
+      <v-card flat class="pa-4">
+        <v-btn success @click="submit()">Simpan</v-btn>
+        <v-btn error @click="cancel()">Batal</v-btn>
+      </v-card>
+    </v-form>
+  </div>
+</template>
+
+<script>
+import moment from "moment";
+export default {
+  props: {
+    data: {},
+    isEdit: {
+      type: Boolean,
+      default: function() {
+        return false;
+      }
+    },
+    onSubmitted: {},
+    onCancel: {}
+  },
+  data: () => ({
+    input: {
+      nomorPO: "",
+      tanggal: new Date(),
+      statusBayar: "",
+      qty: "",
+      jenisPembayaran: "",
+      harga: "",
+      top: ""
+    }
+  }),
+  created() {
+    if (this.isEdit) {
+      this.input = {
+        nomorPO: this.data["nomor_po"],
+        tanggal: moment(this.data["tanggal"]),
+        statusBayar: this.data["status_bayar"],
+        qty: this.data["qty"],
+        jenisPembayaran: this.data["jenis_bayar"],
+        harga: this.data["harga"],
+        top: this.data["top"]
+      };
+    }
+  },
+  methods: {
+    submit() {
+      this.$validator.validateAll().then(() => {
+        if (!this.errors.any()) {
+          this.$http
+            .post("base/service/executeMutation", this.pembelianParams)
+            .then(response => {
+              if (response.data.success_message) {
+                if (this.onSubmitted) this.onSubmitted();
+              }
+            });
+        } else {
+          $(`[name="${this.errors.items[0].field}"]`).focus();
+        }
+      });
+    },
+    cancel() {
+      if (this.onCancel) this.onCancel();
+    },
+    formatCurrency(value) {
+      let val = (value / 1).toFixed(0).replace(".", ",");
+      return val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+    }
+  },
+  computed: {
+    jatuhTempoFormat() {
+      return moment(this.jatuhTempo).format("DD MMMM YYYY");
+    },
+    jatuhTempo() {
+      let jatuhTempo = this.input.tanggal;
+      if (this.input.top) {
+        jatuhTempo = moment(this.input.tanggal).add(this.input.top, "days");
+      }
+      return moment(jatuhTempo).format("YYYY-MM-DD");
+    },
+    mutationType() {
+      let mutationType = this.isEdit ? "update" : "insert";
+      return mutationType;
+    },
+    pembelianData() {
+      let data = {
+        nomor_po: this.input["nomorPO"],
+        tanggal_pembelian: this.input["tanggal"],
+        qty: this.input["qty"],
+        harga: this.input["harga"],
+        jenis_bayar: this.input["jenisPembayaran"],
+        top: this.input["top"],
+        jatuh_tempo: this["jatuhTempo"],
+        status_bayar: this.input["statusBayar"]
+      };
+      return data;
+    },
+    pembelianParams() {
+      let params = {
+        id: undefined,
+        type: this.mutationType,
+        name: "pembelian",
+        data: this.pembelianData
+      };
+      return params;
+    }
+  }
+};
+</script>
+
+<style lang="css">
+
+</style>
